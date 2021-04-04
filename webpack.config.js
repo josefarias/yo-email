a103aac909359a9c20d1d1bd6664da9fa942f95a
Jose Farias <jose@farias.mx>
2021-04-04 13:20:53 -0500

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+const path = require('path')
+const isDev = process.env.NODE_ENV === 'development'
+
+module.exports = {
+  mode: isDev ? 'development' : 'production',
+  entry: './src/index.js',
+  output: {
+    filename: 'main.bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        enforce: 'pre',
+        use: ['source-map-loader'],
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        use: ["style-loader", "css-loader", "sass-loader"]
+      }
+    ]
+  }
+}
