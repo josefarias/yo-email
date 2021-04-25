9a272d7748cb2258cd8ca33840ad43143112b14d
Jose Farias <jose@farias.mx>
2021-04-24 21:02:17 -0500

    Add loader

--- src/server/controllers/ApplicationController.js
+++ src/server/controllers/ApplicationController.js
@@ -19,4 +19,9 @@ export class ApplicationController {
       this.res.end()
     })
   }
+
+  // Simulates a slow network
+  sleep(n) {
+    Atomics.wait(new Int32Array(new SharedArrayBuffer(4)), 0, 0, n * 1000)
+  }
 }
--- src/server/controllers/NavigationController.js
+++ src/server/controllers/NavigationController.js
@@ -2,6 +2,7 @@ import { ApplicationController } from "./ApplicationController"
 
 export class NavigationController extends ApplicationController {
   newAction() {
+    this.sleep(0.8)
     this.renderHTML("navigation.html")
   }
 }
